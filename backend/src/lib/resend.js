e2b6fad36fe45d45a58512465ffb2784274a9d73
omsingh06 <omsinghhw22@gmail.com>
2025-10-27 23:26:22 +0530

    login redevolping

--- backend/src/lib/resend.js
+++
@@ -1,9 +0,0 @@
-import { Resend } from 'resend';
-import {ENV} from './env.js';
-
-export const resendClient = new Resend(ENV.RESEND_API_KEY);
-
-export const sender = {
-  name: ENV.EMAIL_FROM_NAME,
-  email: ENV.EMAIL_FROM,
-};
