0975ef995e90c2f3d9ef23804eb739ba3e1cf114
omsingh <omsinghhw22@gmail.com>
2025-10-28 19:52:55 +0530

    Revert "Login logout"

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,10 +8,6 @@ import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
-  if(!email || !password ){
-    return res.status(400).json({ message: "Email and password are required" });
-  }
-
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
