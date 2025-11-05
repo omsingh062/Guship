17df23d20aa18e379c3f67321ccd73545ef8a180
omsingh06 <omsinghhw22@gmail.com>
2025-11-05 11:14:24 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
-
-axiosInstance.defaults.headers.common["User-Agent"] =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
-axiosInstance.defaults.headers.common["Accept"] = "application/json";
-axiosInstance.defaults.headers.common["Referer"] = "http://localhost:5173/";
- 
-export default axiosInstance;
