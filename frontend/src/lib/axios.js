6cad5d40b73565bbdb9b83c80693471bebf7f66c
omsingh06 <omsinghhw22@gmail.com>
2025-11-05 16:57:34 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     (import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
+      ? "http://localhost:8080/api"
       : "/api"),
   withCredentials: true,
 });
