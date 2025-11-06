6bdd7e466b1f4c403e84ff15c89dcf8721438bf9
omsingh06 <omsinghhw22@gmail.com>
2025-11-06 15:16:15 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://guships.onrender.com";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
